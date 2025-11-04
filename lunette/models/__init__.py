223a6177a96ac6dafd6ddc0251e7f7ae46eb62c0
Leni Shor <elenishor@proton.me>
2025-11-04 16:15:36 -0500

    style: remove slop re-exporting

--- lunette/models/__init__.py
+++ lunette/models/__init__.py
@@ -1,6 +1,1 @@
 """Lunette models for working with Inspect AI trajectories."""
-
-from lunette.models.run import Run
-from lunette.models.trajectory import Trajectory, ScalarScore
-
-__all__ = ["Run", "Trajectory", "ScalarScore"]
